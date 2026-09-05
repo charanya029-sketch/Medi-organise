1dbd041fcd09332851a27a55036241ae19f78a45
charanya029-sketch <charanya029@gmail.com>
2026-09-05 18:29:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -70,8 +70,8 @@ def create_app(config_class=Config):
         db.create_all()
 
     return app
-    app = create_app()
+   
 
 if __name__ == "__main__":
-   
+    app = create_app()
     app.run(host="127.0.0.1", port=5000, debug=True)
